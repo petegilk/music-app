fbe3bc887c9c2a3318e3e124aa436f537119f7f2
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-08-21 19:37:55 -0500

    updated login logic and notification issues

--- src/LogIn.js
+++ src/LogIn.js
@@ -8,24 +8,51 @@ class LogIn extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      loggedIn: false
-    }
+      loggedIn: false,
+      username: '',
+      password: ''
+   };
+  };
+
+  clickHandler = (e) => {
+   // e.preventDefault();
+    //console.log(this.state)
+
+    if (this.state.username === '') {
+      alert(`You haven't entered a username yet`) 
+    } else if (this.state.password === '') {
+      alert(`You must have a password`)
+    } else if (this.state.username !== '' && this.state.password !== '') {
+    this.setState({
+      loggedIn: !this.state.loggedIn,
+    })
+  }
+    console.log(this.state)
+  };
+
+  handleName = e => {
+      this.setState({
+        username: e.target.value,
+      })
   }
 
-  clickHandler = () => {
+  handlePassword = e => {
     this.setState({
-      loggedIn: !this.state.loggedIn
+      password: e.target.value
     })
   }
 
-  render() {
-    return this.state.loggedIn ? <Dashboard /> : (
+  render() { 
+  
+    return this.state.loggedIn ? <Dashboard user={this.state.username}/> : (
       <div>
         <NavBar />
           <Card className="logInCard">
             <CardContent>
-               <TextField label="Username *" /><br />
-               <TextField label="Password *" /><br />
+               <TextField onChange={this.handleName} label="Username *" />
+               <br />
+               <TextField onChange={this.handlePassword} type="password" id="outlined-password-input" label="Password *" />
+               <br />
                <Button className="logInButton" onClick={this.clickHandler}>Log In</Button>
             </CardContent>
           </Card>
